358f6a2373f4ccce845372683182d98a06fa91ef
jackyang1989 <87349221+jackyang1989@users.noreply.github.com>
2025-11-10 08:20:10 +0800

    Create __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.admin import AdminUser
+from app.models.service import Service
+from app.models.user import User
+from app.models.domain import Domain
+
+__all__ = ["AdminUser", "Service", "User", "Domain"]
